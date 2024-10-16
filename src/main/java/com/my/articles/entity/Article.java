085d36742a4f1c5f4246651973d9af87b15d440e
HYUNJEONG KIM <162420530+hjkim57@users.noreply.github.com>
2024-10-16 15:21:54 +0900

    양방향 연관관계 처리 종료 (#8)

--- src/main/java/com/my/articles/entity/Article.java
+++ src/main/java/com/my/articles/entity/Article.java
@@ -3,6 +3,9 @@ package com.my.articles.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 public class Article {
@@ -14,6 +17,9 @@ public class Article {
     private String title;
     @Column(nullable = false, length = 1000)
     private String content;
+
+    @OneToMany(mappedBy = "article", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.LAZY)
+    private List<Comment> comments = new ArrayList<>();
 
 
 }
